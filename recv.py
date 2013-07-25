3b163e93b88cdaf16d83c4d0b87ef5bee59e917f
roy <roy.lieu@gmail.com>
2013-07-25 10:44:00 +0800

    first commit

---
+++ recv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import pika
+import json
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+	        host='localhost'))
+channel = connection.channel()
+
+#channel.exchange_declare(exchange='nagios', type='fanout')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='nagios',
+		                   queue=queue_name)
+
+print ' [*] Waiting for logs. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+        body = json.loads(body)
+        print " [x] %r" % (body,)
+
+channel.basic_consume(callback, queue=queue_name, no_ack=True)
+
+channel.start_consuming()
